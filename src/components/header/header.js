ba4febcae3f3bc870e3e5830d0b30f7639fcc2a9
Kirill Ivashkin <kirillkus@yandex.ru>
2021-07-30 18:38:28 +0300

    v. 0.0.7  Added responsive layout for Home Page

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -13,6 +13,16 @@ const Header = () => {
         <p><Link to="/">О НАС</Link></p>
         <p><Link to="/">КОНТАКТЫ</Link></p>
       </nav>
+      <button className="header__menu">
+        <div className="header__menu-stripe"></div>
+        <div className="header__menu-stripe"></div>
+        <div className="header__menu-stripe"></div>
+      </button>
+      <div className="header__socials">
+        <Link to="/facebook" className="header__socials-item header__socials-facebook" target="_blank"></Link>
+        <Link to="/twitter" className="header__socials-item header__socials-twitter" target="_blank"></Link>
+        <Link to="/instagram" className="header__socials-item header__socials-instagram" target="_blank"></Link>
+      </div>
     </div>
   );
 };
